9433732a902bcd1d22bc7e708fdda87984a285d9
VuNgocThanhTruc <vutruc0702@gmail.com>
2022-12-16 22:36:10 +0700

    Truc: merge branch truc with branch main

--- src/main/java/vn/edu/hcmuaf/fit/controller/web/LoginController.java
+++ src/main/java/vn/edu/hcmuaf/fit/controller/web/LoginController.java
@@ -11,9 +11,10 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.lang.System.*;
+
 @WebServlet("/login")
 public class LoginController extends HttpServlet {
-    private static  final long serialVersionUID =1L;
+    private static final long serialVersionUID = 1L;
 
     public LoginController() {
         super();
@@ -28,19 +29,19 @@ public class LoginController extends HttpServlet {
         String action = request.getParameter("action");
         String username = request.getParameter("username");
         String password = request.getParameter("password");
-        System.out.println( "doget: "+action);
+        System.out.println("doget: " + action);
         if (action == null) {
 
             System.out.println("Khong thuc hien duoc gi het");
 
-        }
-        else if (action.equals("logout")) {
+        } else if (action.equals("logout")) {
             HttpSession session = request.getSession();
             session.invalidate();
 //                response.sendRedirect("signin");
             response.sendRedirect("signin");
         }
     }
+
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.setCharacterEncoding("UTF-8");
@@ -50,7 +51,8 @@ public class LoginController extends HttpServlet {
         String action = request.getParameter("action");
         String username = request.getParameter("username");
         String password = request.getParameter("password");
-        System.out.println( "dopost: "+action);
+        String hovaten = request.getParameter("hovaten");
+        System.out.println("dopost: " + action);
         HttpSession session = request.getSession();
         session.setAttribute("mess", null);
         if (action == null) {
@@ -58,23 +60,43 @@ public class LoginController extends HttpServlet {
             session.setAttribute("mess", null);
         }
         else if (action.equals("login")) {
-            if (new UserDAO().checkLogin(username, password)==1) {
+            int check =new UserDAO().checkLogin(username, password);
+            if (check== 1) {
                 User user = UserDAO.mapUser.get(username);
                 session.setAttribute("userlogin", user);
                 session.setAttribute("mess", null);
                 response.sendRedirect("home");
-            }
-            else if (new UserDAO().checkLogin(username, password)==2 ) {
+            } else if (check== 2) {
 
                 session.setAttribute("mess", null);
                 response.sendRedirect("index");
-            }
-           else {
-                session.setAttribute("mess", "error");
+            } else {
+                session.setAttribute("mess", "errorsignin");
                 response.sendRedirect("signin");
             }
-        }
-        else{
+        } else if (action.equals("logout")) {
+            int check = new UserDAO().checksignup(hovaten,username,password);
+            if (check ==1){
+                new UserDAO().signup(hovaten,username,password);
+                request.getRequestDispatcher("/view/web/signin.jsp").forward(request,response);
+                System.out.println("a " );
+            }
+            else if (check==2){
+                session.setAttribute("mess", "errornull");
+                response.sendRedirect("signup");
+                System.out.println("b " );
+            }
+            else {
+                session.setAttribute("mess", "errornull");
+                session.setAttribute("mess", "errorsignup");
+                response.sendRedirect("signup");
+
+            }
+//            response.sendRedirect("signin");
+            System.out.println("c " );
+//            request.getRequestDispatcher("/view/web/signin.jsp").forward(request,response);
+        } else {
+            request.getRequestDispatcher("/view/web/signin.jsp").forward(request,response);
             session.setAttribute("mess", null);
         }
 
