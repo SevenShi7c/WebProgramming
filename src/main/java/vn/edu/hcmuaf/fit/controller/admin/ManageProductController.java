77eb4dc340a3ddd34432fb232210e6a1a861012d
VuNgocThanhTruc <vutruc0702@gmail.com>
2022-12-17 01:28:31 +0700

    Truc:[page admin] process add new product

--- src/main/java/vn/edu/hcmuaf/fit/controller/admin/ManageProductController.java
+++ src/main/java/vn/edu/hcmuaf/fit/controller/admin/ManageProductController.java
@@ -2,6 +2,7 @@ package vn.edu.hcmuaf.fit.controller.admin;
 
 import vn.edu.hcmuaf.fit.constant.SystemConstant;
 import vn.edu.hcmuaf.fit.model.ProductModel;
+import vn.edu.hcmuaf.fit.service.CategorySevice;
 import vn.edu.hcmuaf.fit.service.ProductService;
 
 import javax.servlet.ServletException;
@@ -13,30 +14,29 @@ import java.io.IOException;
 
 @WebServlet(name = "ManageProductController", value = "/admin/manage-product")
 public class ManageProductController extends HttpServlet {
-    private String view = "";
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        ProductModel productModel = new ProductModel();
-        ProductService productService = new ProductService();
         String urlTypeParam = request.getParameter("type");
+        String view = "";
+        ProductService productService = new ProductService();
+        ProductModel productModel = new ProductModel();
+        CategorySevice categorySevice = new CategorySevice();
 
         if (SystemConstant.LIST.equals(urlTypeParam)) {
             view = "/view/admin/manage-product.jsp";
         } else if (SystemConstant.ADD.equals(urlTypeParam)) {
-            view = "/view/admin/add-product.jsp";
+            view = "/view/admin/change-product.jsp";
         } else if (SystemConstant.EDIT.equals(urlTypeParam)) {
             if (productModel.getId() != null) {
                 productService.getDetailProduct(productModel.getId());
-                view = "/view/admin/edit-product.jsp";
-            } else {
-                view = "/view/admin/add-product.jsp";
             }
-        }else if(urlTypeParam==null){
+            view = "/view/admin/change-product.jsp";
+        } else if (urlTypeParam == null) {
             view = "/view/admin/manage-product.jsp";
         }
 
-        System.out.println(view);
+        request.setAttribute("categoryBrand",categorySevice.getListBrand());
         request.setAttribute("productModel", productModel);
         request.getRequestDispatcher(view).forward(request, response);
     }
